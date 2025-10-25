6a9e626cc0a264f40c524fc8b6b9ff11ff608ddd
aleksandrShukman <aleksandrshukman@protonmail.com>
2025-10-25 22:38:43 +0200

    Initial Commit

---
+++ acPulse_vcLBM/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  acPulse_vcLBM
+//
+//  Created by Alexander Schukmann on 25.10.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
